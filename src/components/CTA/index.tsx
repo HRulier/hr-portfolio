92ebb53d6fa56c1cb5a8f77bc339ce8bb735f701
HRulier <hugo.rulier@gmail.com>
2026-02-09 18:52:28 +0100

    feat(3.2, 4): useMousePosition + CTA dot follow cursor

--- src/components/CTA/index.tsx
+++ src/components/CTA/index.tsx
@@ -1,11 +1,124 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import useMousePosition from "@/hooks/useMousePosition";
 import styles from "./cta.module.scss";
 
+// const LERP = 0.12;
+// const OFFSET = 5;
+
 export default function CTA() {
+  const containerRef = useRef<HTMLElement>(null);
+  const dotRef = useRef<HTMLSpanElement>(null);
+  const isInsideRef = useRef(false);
+  const dotOriginalCenterRef = useRef({ x: 0, y: 0 });
+  const mouseRef = useRef({ x: 0, y: 0 });
+
+  const { isInside } = useMousePosition({
+    containerRef,
+    onMouseMove: ({ x, y }) => {
+      mouseRef.current = { x, y };
+    },
+  });
+
+  const getDotCenter = () => {
+    const d = dotRef.current?.getBoundingClientRect();
+    const c = containerRef.current?.getBoundingClientRect();
+    if (!d || !c) return { x: 0, y: 0 };
+
+    return {
+      x: d.left + d.width / 2 - c.left || 0,
+      y: d.top + d.height / 2 - c.top || 0,
+    };
+  };
+
+  useEffect(() => {
+    isInsideRef.current = isInside;
+  }, [isInside]);
+
+  // Set original dot position
+  useEffect(() => {
+    dotOriginalCenterRef.current = getDotCenter();
+    const onResize = () => {
+      dotOriginalCenterRef.current = getDotCenter();
+    };
+
+    window.addEventListener("resize", onResize, { passive: true });
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const dot = dotRef.current;
+    if (!dot) return;
+
+    const position = {
+      x: 0,
+      y: 0,
+    };
+
+    const animate = () => {
+      const { x: mouseX, y: mouseY } = mouseRef.current;
+
+      const dotCenter = getDotCenter();
+
+      // define the target position
+
+      let targetX = mouseX;
+      let targetY = mouseY;
+
+      if (!isInsideRef.current) {
+        targetX = dotOriginalCenterRef.current.x;
+        targetY = dotOriginalCenterRef.current.y;
+      }
+
+      //
+
+      const dist = {
+        x: targetX - dotCenter.x,
+        y: targetY - dotCenter.y,
+      };
+
+      position.x += dist.x * 0.2;
+      position.y += dist.y * 0.2;
+
+      const t = Math.min(Math.abs(dist.x), 100) / 100;
+      const k = isInsideRef.current ? 0.5 + t * 0.7 : 1;
+
+      if (
+        !isInsideRef.current &&
+        Math.abs(position.x) < 0.5 &&
+        Math.abs(position.y) < 0.5
+      ) {
+        position.x = 0;
+        position.y = 0;
+        dot.style.transform = "";
+      } else {
+        dot.style.transform = `translate(${position.x}px, ${position.y}px) scale(${k}) `;
+      }
+
+      rafId = requestAnimationFrame(animate);
+    };
+
+    let rafId = requestAnimationFrame(animate);
+
+    return () => {
+      dot.style.transform = "translate(0px, 0px)";
+      cancelAnimationFrame(rafId);
+      dotOriginalCenterRef.current = getDotCenter() || { x: 0, y: 0 };
+    };
+  }, []);
+
   return (
-    <section className={styles.cta}>
+    <section className={styles.cta} ref={containerRef}>
       <div className={styles.content}>
         <h2 className={styles.title}>
-          Travaillons ensemble<span className={styles.dot}>.</span>
+          Travaillons ensemble
+          <span className={styles.dot} ref={dotRef}>
+            .
+          </span>
         </h2>
         <p>Mon profil vous intéresse ?</p>
         <a href="#" className={styles.btnCta}>
@@ -17,9 +130,9 @@ export default function CTA() {
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M17 8l4 4m0 0l-4 4m4-4H3"
             />
           </svg>
