5d1e5bb602eae3ecf6b8a9dfc3f8ba7c1d98afe9
HRulier <hugo.rulier@gmail.com>
2026-03-11 18:14:08 +0100

    feat(projects page): create projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,17 @@
 import styles from "./projects.module.scss";
 
 export default function Projects() {
-  return <section>Projects</section>;
+  return (
+    <div className={styles.projects}>
+      <section className={styles.hero}>
+        <div className={styles.content}>
+          <div className={styles.line}>
+            <h1>
+              Projets<span className={styles.dot}>.</span>
+            </h1>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
