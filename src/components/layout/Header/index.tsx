ac9522224160afa93869c95e3aadc6e5b523f38f
HRulier <hugo.rulier@gmail.com>
2026-04-20 17:30:17 +0200

    feat(header): add download cv

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { MdFileDownload } from "react-icons/md";
 import styles from "./header.module.scss";
 import { TransitionLink } from "@/components/layout/PageTransition";
 
@@ -34,6 +35,16 @@ const Header = () => {
           ) : (
             <TransitionLink href="/#stack">Compétences</TransitionLink>
           )}
+        </li>
+        <li>
+          <a
+            className={styles.download}
+            href="documents/CV_Hugo_RULIER.pdf"
+            download="CV_Hugo_RULIER.pdf"
+          >
+            <MdFileDownload size={24} />
+            <span>CV</span>
+          </a>
         </li>
         <li className={styles.cta}>
           <TransitionLink href="/contact">Contact</TransitionLink>
